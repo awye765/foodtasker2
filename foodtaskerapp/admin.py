a55fa9e1c74731fe87b39660fd39620cc9ebc157
awye765 <awye765@googlemail.com>
2020-12-01 21:53:45 +0000

    Added order model & order details model  & tested with dummy orders and order details

--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from foodtaskerapp.models import Restaurant, Customer, Driver, Meal
+from foodtaskerapp.models import Restaurant, Customer, Driver, Meal, Order, OrderDetail
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
 
 
