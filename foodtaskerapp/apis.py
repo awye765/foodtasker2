ec48d2302576c35132402bd47324adf362f4d78f
awye765 <awye765@googlemail.com>
2020-12-02 11:29:41 +0000

    Add api endpoint for restaurants

---
+++ foodtaskerapp/apis.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+
+from foodtaskerapp.models import Restaurant
+from foodtaskerapp.serializers import RestaurantSerializer
+
+# API function to retrieve restaurants data in Json
+def customer_get_restaurants(request):
+    restaurants = RestaurantSerializer(
+        Restaurant.objects.all().order_by("-id"),
+        many = True
+    ).data
+
+    return JsonResponse({"restaurants": restaurants})
