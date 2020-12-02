ed8dc86bbfe9c7bfd09e2804034707760a3e8492
awye765 <awye765@googlemail.com>
2020-12-02 20:54:05 +0000

    Added api functionality to complete an order once driver delivers

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -174,9 +174,23 @@ def driver_get_the_latest_order(request):
 
     return JsonResponse({"order": order})
 
+# POST params: access_token
+@csrf_exempt
 def driver_complete_order(request):
+    # Get token
+    access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+        expires__gt = timezone.now())
 
-    return JsonResponse({})
+    # Get Driver
+    driver = access_token.user.driver
+
+    # Get Order
+    order = Order.objects.get(id = request.POST["order_id"], driver = driver)
+
+    order.status = Order.DELIVERED
+    order.save()
+
+    return JsonResponse({"status": "success"})
 
 def driver_get_revenue(request):
 
