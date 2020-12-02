c76ccdb2c2d7ca091cdd84154ace453553afcb72
awye765 <awye765@googlemail.com>
2020-12-02 13:26:25 +0000

    Added to admin views the ids of meal, oder and order detail

--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -3,11 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from foodtaskerapp.models import Restaurant, Customer, Driver, Meal, Order, OrderDetail
 
+class MealAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+class OrderDetailAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Driver)
-admin.site.register(Meal)
-admin.site.register(Order)
-admin.site.register(OrderDetail)
-
-
+admin.site.register(Meal, MealAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderDetail, OrderDetailAdmin)
