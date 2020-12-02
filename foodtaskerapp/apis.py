65d3c30aa162e5f9337202f6c112aebaff420b2b
awye765 <awye765@googlemail.com>
2020-12-02 18:17:24 +0000

    Create general structure for Driver apis - functions empty for now:

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -7,7 +7,11 @@ from django.views.decorators.csrf import csrf_exempt
 from oauth2_provider.models import AccessToken
 
 from foodtaskerapp.models import Restaurant, Meal, Order, OrderDetail
-from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer, OrderSerializer
+from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer,
+
+##############
+# CUSTOMERS
+##############
 
 # API function to retrieve restaurants data in Json
 def customer_get_restaurants(request):
@@ -98,8 +102,31 @@ def customer_get_latest_order(request):
 
     return JsonResponse({"order": order})
 
+##############
+# RESTAURANT
+##############
+
 def restaurant_order_notification(request, last_request_time):
     notification = Order.objects.filter(restaurant = request.user.restaurant,
         created_at__gt = last_request_time).count()
 
     return JsonResponse({"notification": notification})
+
+##############
+# DRIVERS
+##############
+
+def driver_get_ready_orders(request):
+    return JsonResponse({})
+
+def driver_pick_order(request):
+    return JsonResponse({})
+
+def driver_get_the_latest_order(request):
+    return JsonResponse({})
+
+def driver_complete_order(request):
+    return JsonResponse({})
+
+def driver_get_revenue(request):
+    return JsonResponse({})
